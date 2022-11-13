3c0fa2becc3d335ba87d99f6f2be58c80f9b483a
Jonah Burgos <burgosjonah@gmail.com>
2022-11-13 12:22:33 -0500

    added filter and modified xml

--- AutomataProject.py
+++ AutomataProject.py
@@ -2,33 +2,63 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import xml.etree.ElementTree as ET
+import os
+
+########################
+# FUNCTION DEFINITIONS #
+########################
+
+# Regex-based parsing
+# Fetches page content and parses for:
+#  - Emails
+#  - Phone numbers
+# Returns a jagged array of data in the order listed above
+def parsePage(url):
+    page_fetch = requests.get(url)
+    page_source = str(BeautifulSoup(page_fetch.content, 'html.parser'))
+    email_matches = re.findall(re_emailLink, page_source)
+    phone_matches = re.findall(re_phoneNumber, page_source)
+
+    return (email_matches, phone_matches)
+
+# Appends a jagged array data matrix to an XML parent node
+# Each row index in the matrix must correlate to each index of elementList
+# Elements of each matrix row are appended to their respective elementList
+# Returns nothing
+def appendToXML(parent, dataMatrix, elementList):
+    for i, dataRow in enumerate(dataMatrix):
+        tag = elementList[i].tag.split('_')[0]
+        for dataCol in dataRow:
+            ET.SubElement(elementList[i], tag).text = dataCol
+
+
+################
+#     MAIN     #
+################
 
 # compile regex pattern into regex objects
 re_emailLink = re.compile(r"(?<=mailto:)[0-9a-zA-Z.+-_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}")  # matches the email format defined in RFC
 re_phoneNumber = re.compile(r"\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}")  # matches the standard email formats
 re_aspx = re.compile(r"\.aspx$")  # matches .aspx websites
 
-# accessing URl
+# accessing root URl
 url_root = "https://www.usf.edu/engineering/about/deans-office.aspx"
 print("Requesting access to: '%s'" % url_root)
-page = requests.get(url_root)
-if not page.ok:
-    print("An error has occurred with the entered link: %d" % page.status_code)
+page_fetch = requests.get(url_root)
+if not page_fetch.ok:
+    print("An error has occurred with the entered link: %d" % page_fetch.status_code)
     exit(1)
 print("Request granted")
 
 # initialize XML output
 data = ET.Element('URLGraphXML')
-# tree = ET.ElementTree(xml_root)
+tree = ET.ElementTree(data)
 
+# extract page content into BeautifulSoup object
+page_source = BeautifulSoup(page_fetch.content, 'html.parser')
 
-
-
-# Parse Webpage with Beautiful soup object
-page_source = BeautifulSoup(page.content, 'html.parser')
-
-# Parse root page for subpage links
-# Filter non '/engineering' subpages
+# parse root page for subpage links
+# filter non '/engineering' webpages (ASPX)
 subpages = [url_root]  # root is first element
 for tag in page_source.find_all(href=re.compile("^/engineering")):  # locate all engineering pages
     if '/engineering' in tag['href']:
@@ -41,45 +71,59 @@ print("    (root)%s\n    " % subpages[0], end='')
 print(*subpages[1:], sep="\n    ")
 print("Pages found (%d)" % len(subpages))
 
-def parsePage(element, url):
-    email_matches = re.findall(re_emailLink, page_source)
-    phone_matches = re.findall(re_phoneNumber, page_source)
-    return
 
+# Collect data for root page
+page_source = str(page_source)
 root_page = ET.SubElement(data, 'root', url=url_root)
-data.append(root_page)
+email_matches = re.findall(re_emailLink, page_source)
+phone_matches = re.findall(re_phoneNumber, page_source)
+data_matrix = (email_matches, phone_matches)
+email_list = ET.SubElement(root_page, 'email_list')
+phone_list = ET.SubElement(root_page, 'phone_list')
+element_list = (email_list, phone_list)
 
+appendToXML(root_page, data_matrix, element_list)
+print("\nRoot URL: %s" % url_root)
+print("    Emails: ", end='')
+print(data_matrix[0])
+print("    Phone#: ", end='')
+print(data_matrix[1])
 
 # parse page source for regex matches using regex object
-print("\nRoot URL: " + url_root)
-i = 1
-for url in subpages:
-    page = requests.get(url)
-    page_source = str(BeautifulSoup(page.content, 'html.parser'))
-    email_matches = re.findall(re_emailLink, page_source)
-    phone_matches = re.findall(re_phoneNumber, page_source)
-    if i == 1:
-        print("    Emails: ", end='')
-        print(email_matches)
-        print("    Phone#: ", end='')
-        print(phone_matches)
-        i = 0
-    if not email_matches and not phone_matches:
-        continue
-    print("Link: "+url)
-    Child_URL = ET.fromstring("<URL = >"+str(url)+"</URL>")
-    root_URL.append(Child_URL)
-    print("    Emails: ", end='')
-    print(email_matches)
-    Email = ET.fromstring("<email>"+str(email_matches)+"</email>")
-    Child_URL.append(Email)
-    print("    Phone#: ", end='')
-    print(phone_matches)
-    Phone = ET.fromstring("<phone>"+str(phone_matches)+"</phone>")
-    Child_URL.append(Phone)
+for url in subpages[1:]:
+    # create url node
+    subpage = ET.SubElement(root_page, 'subpage', url=url)
 
-exit(0)
-print("Generating URL Graph...")
-tree = ET.ElementTree(xml_root)
-tree.write("output.xml")
-print("Graph successfully generated.")
+    # parse url for data
+    data_matrix = parsePage(url)
+
+    # skip url if parsing returned no data
+    if not data_matrix:
+        continue
+
+    # create sub elements to categorize data
+    email_list = ET.SubElement(subpage, 'email_list')
+    phone_list = ET.SubElement(subpage, 'phone_list')
+    element_list = (email_list, phone_list)
+
+    # append data matrix to xml tree
+    appendToXML(subpage, data_matrix, element_list)
+
+    # print data to console
+    print("Link: %s" % url)
+    print("    Emails: ", end='')
+    print(data_matrix[0])
+    print("    Phone#: ", end='')
+    print(data_matrix[1])
+
+# generate xml tree
+print("\nGenerating URL Graph...")
+
+
+# write tree to file
+filename = "output.xml"
+tree.write(filename)
+
+# print file link to console
+dir_path = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
+print("Graph successfully generated: file:///"+dir_path+"/%s" % filename)
